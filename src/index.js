29f040282087bc73c1c7c6b53320112782db6902
snoor_madih <snoor.madih@gmail.com>
2021-06-23 08:23:50 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
-// <<<<<<< HEAD
 ReactDOM.render(< App />, document.getElementById('container'));
-// =======
-// ReactDOM.render(< App />, document.getElementById('container'))
-// // >>>>>>> b4d02df20c24ad1220677ce15a81ca24cac0f9f6
